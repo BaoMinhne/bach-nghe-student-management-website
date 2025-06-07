e5f98a54bf473452ef153a77ad511b8680df83fc
BaoMinhne <130214731+minhb2105709@users.noreply.github.com>
2025-06-07 17:47:58 +0700

    Adjust the logic in database, then fix bug in student and teacher

--- backend/src/models/student.model.js
+++ backend/src/models/student.model.js
@@ -4,18 +4,23 @@ const student = {
   getAllSubjectScore: async (studentCode) => {
     const result = await knex("student as s")
       .join("class_student as cs", "s.student_code", "cs.student_code")
-      .join("class as c", "cs.class_id", "c.class_id")
+      .join(
+        "class_subject as csub",
+        "cs.class_subject_id",
+        "csub.class_subject_id"
+      )
+      .join("class as c", "csub.class_id", "c.class_id")
+      .join("module as m", "csub.module_id", "m.module_id")
+      .join("semester as se", "csub.semester_id", "se.semester_id")
       .leftJoin("score as sc", function () {
-        this.on("s.student_code", "=", "sc.student_code").andOn(
-          "c.class_id",
+        this.on("cs.class_subject_id", "=", "sc.class_subject_id").andOn(
+          "s.student_code",
           "=",
-          "sc.class_id"
+          "sc.student_code"
         );
       })
-      .join("semester as se", "sc.semester_id", "se.semester_id")
-      .leftJoin("module as m", "sc.module_id", "m.module_id")
       .where("s.student_code", studentCode)
-      .select(
+      .distinct(
         "s.student_code as MÃ HS",
         "s.student_middle_name as HỌ ĐỆM",
         "s.student_name as TÊN",
@@ -34,19 +39,24 @@ const student = {
   filterSubjectScoreBySemester: async (studentCode, semester) => {
     const result = await knex("student as s")
       .join("class_student as cs", "s.student_code", "cs.student_code")
-      .join("class as c", "cs.class_id", "c.class_id")
+      .join(
+        "class_subject as csub",
+        "cs.class_subject_id",
+        "csub.class_subject_id"
+      )
+      .join("class as c", "csub.class_id", "c.class_id")
+      .join("module as m", "csub.module_id", "m.module_id")
+      .join("semester as se", "csub.semester_id", "se.semester_id")
       .leftJoin("score as sc", function () {
-        this.on("s.student_code", "=", "sc.student_code").andOn(
-          "c.class_id",
+        this.on("cs.class_subject_id", "=", "sc.class_subject_id").andOn(
+          "s.student_code",
           "=",
-          "sc.class_id"
+          "sc.student_code"
         );
       })
-      .join("semester as se", "sc.semester_id", "se.semester_id")
-      .leftJoin("module as m", "sc.module_id", "m.module_id")
       .where("s.student_code", studentCode)
       .andWhere("se.semester_number", semester)
-      .select(
+      .distinct(
         "s.student_code as MÃ HS",
         "s.student_middle_name as HỌ ĐỆM",
         "s.student_name as TÊN",
